ea2f57ac88af962aeb9b13fdadd9b81fa4eac52c
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-06 17:35:46 -0700

    added variables and ids into html for buttons and questoins

--- Assets/script.js
+++ Assets/script.js
@@ -15,3 +15,14 @@ var questions = [
         answer: "True" 
     },
 ];
+
+// variables
+
+var score = 0;
+var questionIndex = 0;
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
+// 15 seconds for each question
