60d0ea89bb215415475fdb04698c4d5c109e50b2
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-06 17:24:38 -0700

    second time commit cause it didnt work the first time

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        title: "The <script> tag is placed at the top of the page?",
+        choices: ["True", "False"],
+        answer: "False"
+    },
+    {
+        title: "What HTML element is used for the Javascript tag?",
+        choices: ["<java>", "<js>", "<script>", "<javascript>"],
+        answer: "<script>"
+    },
+    {
+        title: "Java and Javascript are the Same thing?",
+        choices: ["True", "False"],
+        answer: "True" 
+    },
+];
