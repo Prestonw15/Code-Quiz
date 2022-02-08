a067cb7861100d95cd805c5718bb45debb3b586c
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-07 18:22:11 -0700

    updating to make sure it works

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -2,7 +2,7 @@ var highScore = document.querySelector("#highScore");
 
 var clear = document.querySelector("#clear");
 
-var goBack = document.querySelector("#goBack");
+var goBack = document.querySelector("#Home");
 clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
