d880f77b7e2afa43f4dce20cf67fca32e08590bd
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-07 17:40:03 -0700

    finish my script and changed the src to match better

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -71,3 +71,89 @@ function render(questionIndex) {
 }
 
 // This will compare your choices to the actual answer
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+        } else {
+            // Penalizes -5 seconds off for wrong answer
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+        }
+
+    }
+
+    questionIndex++;
+
+    if (questionIndex >= questions.length) {
+        // last page with user stats
+        allDone();
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+    } else {
+        render(questionIndex);
+    }
+    questionsDiv.appendChild(createDiv);
+
+}
+
+function allDone() {
+    questionsDiv.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "All Done!"
+
+    questionsDiv.appendChild(createH1);
+
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionsDiv.appendChild(createP);
+
+
+        // calculates time remaining and replaces it with score
+        if (secondsLeft >= 0) {
+            var timeRemaining = secondsLeft;
+            var createP2 = document.createElement("p");
+            clearInterval(holdInterval);
+            createP.textContent = "Your final score is: " + timeRemaining;
+    
+            questionsDiv.appendChild(createP2);
+        } 
+    
+  // event listener to capture initials and local storage for initials and score
+            createSubmit.addEventListener("click", function () {
+            var initials = createInput.value;
+
+            if (initials === null) {
+
+            console.log("No value entered!");
+
+            } else {
+                var finalScore = {
+                    initials: initials,
+                    score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+            // goes to final page
+            window.location.replace("./HighScores.html");
+    }
+});
+
+}
